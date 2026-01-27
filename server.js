1e5129ac01590643bfe68e40182bfd1ce1a3db2b
Stella <stellagmendonca@gmail.com>
2026-01-27 17:51:26 -0300

    mudei server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const app = require("./src/app.js");
+const db = require("./src/models");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Banco de dados sincronizado com sucesso!");
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Falha ao sincronizar o banco de dados:", err);
+  });
